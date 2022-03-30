ca67b248899ada8765e81279020a22f7729c7dbf
marzhn <smakmake@gmail.com>
2022-03-30 22:07:20 +0600

    store - glasses mag
    code in django

---
+++ store.m/onshop/store/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
